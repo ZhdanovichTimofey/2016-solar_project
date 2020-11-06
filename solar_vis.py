b77d8322b6e198b24df6367be2a3c4c721fdb065
ZhdanovichTimofey <71005308+ZhdanovichTimofey@users.noreply.github.com>
2020-11-06 16:46:47 +0300

    Исправил функции scale_y и create_planet_image

--- solar_vis.py
+++ solar_vis.py
@@ -2,8 +2,10 @@
 # license: GPLv3
 
 """Модуль визуализации.
-Нигде, кроме этого модуля, не используются экранные координаты объектов.
-Функции, создающие гaрафические объекты и перемещающие их на экране, принимают физические координаты
+Нигде, кроме этого модуля, не используются экранные координаты
+объектов.
+Функции, создающие графические объекты и перемещающие их на экране,
+принимают физические координаты
 """
 
 header_font = "Arial-16"
@@ -22,9 +24,10 @@ scale_factor = None
 
 
 def calculate_scale_factor(max_distance):
-    """Вычисляет значение глобальной переменной **scale_factor** по данной характерной длине"""
+    """Вычисляет значение глобальной переменной **scale_factor**
+    по данной характерной длине"""
     global scale_factor
-    scale_factor = 0.4*min(window_height, window_width)/max_distance
+    scale_factor = 0.4 * min(window_height, window_width) / max_distance
     print('Scale factor:', scale_factor)
 
 
@@ -39,7 +42,7 @@ def scale_x(x):
     **x** — x-координата модели.
     """
 
-    return int(x*scale_factor) + window_width//2
+    return int(x * scale_factor) + window_width // 2
 
 
 def scale_y(y):
@@ -54,7 +57,7 @@ def scale_y(y):
     **y** — y-координата модели.
     """
 
-    return y  # FIXME: not done yet
+    return int(y * scale_factor) + window_height // 2
 
 
 def create_star_image(space, star):
@@ -69,7 +72,8 @@ def create_star_image(space, star):
     x = scale_x(star.x)
     y = scale_y(star.y)
     r = star.R
-    star.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=star.color)
+    star.image = space.create_oval([x - r, y - r], [x + r, y + r],
+                                   fill=star.color)
 
 
 def create_planet_image(space, planet):
@@ -80,7 +84,11 @@ def create_planet_image(space, planet):
     **space** — холст для рисования.
     **planet** — объект планеты.
     """
-    pass  # FIXME: сделать как у звезды
+    x = scale_x(planet.x)
+    y = scale_y(planet.y)
+    r = planet.R
+    planet.image = space.create_oval([x - r, y - r], [x + r, y + r],
+                                     fill=planet.color)
 
 
 def update_system_name(space, system_name):
@@ -108,7 +116,7 @@ def update_object_position(space, body):
     r = body.R
     if x + r < 0 or x - r > window_width or y + r < 0 or y - r > window_height:
         space.coords(body.image, window_width + r, window_height + r,
-                     window_width + 2*r, window_height + 2*r)  # положить за пределы окна
+                     window_width + 2*r, window_height + 2*r)
     space.coords(body.image, x - r, y - r, x + r, y + r)
 
 
