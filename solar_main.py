be2f31708bee5af3be18f9f479f3b3f411b51944
ZhdanovichTimofey <71005308+ZhdanovichTimofey@users.noreply.github.com>
2020-11-06 22:18:29 +0300

    Программа полностью работоспособна. Проведена проверка на соблюдение PEP8. Можно добавить функцию считывания в файл

--- solar_main.py
+++ solar_main.py
@@ -6,7 +6,6 @@ from tkinter.filedialog import *
 from solar_vis import *
 from solar_model import *
 from solar_input import *
-from solar_objects import *
 
 perform_execution = False
 """Флаг цикличности выполнения расчёта"""
@@ -28,10 +27,13 @@ space_objects = []
 
 
 def execution():
-    """Функция исполнения -- выполняется циклически, вызывая обработку всех небесных тел,
+    """Функция исполнения -- выполняется циклически, вызывая обработку
+    всех небесных тел,
     а также обновляя их положение на экране.
-    Цикличность выполнения зависит от значения глобальной переменной perform_execution.
-    При perform_execution == True функция запрашивает вызов самой себя по таймеру через от 1 мс до 100 мс.
+    Цикличность выполнения зависит от значения глобальной переменной
+    perform_execution.
+    При perform_execution == True функция запрашивает вызов самой себя
+    по таймеру через от 1 мс до 100 мс.
     """
     global physical_time
     global displayed_time
@@ -104,7 +106,8 @@ def save_file_dialog():
 
 def main():
     """Главная функция главного модуля.
-    Создаёт объекты графического дизайна библиотеки tkinter: окно, холст, фрейм с кнопками, кнопки.
+    Создаёт объекты графического дизайна библиотеки tkinter: окно,
+    холст, фрейм с кнопками, кнопки.
     """
     global physical_time
     global displayed_time
@@ -118,13 +121,15 @@ def main():
 
     root = tkinter.Tk()
     # космическое пространство отображается на холсте типа Canvas
-    space = tkinter.Canvas(root, width=window_width, height=window_height, bg="black")
+    space = tkinter.Canvas(root, width=window_width, height=window_height,
+                           bg="black")
     space.pack(side=tkinter.TOP)
     # нижняя панель с кнопками
     frame = tkinter.Frame(root)
     frame.pack(side=tkinter.BOTTOM)
 
-    start_button = tkinter.Button(frame, text="Start", command=start_execution, width=6)
+    start_button = tkinter.Button(frame, text="Start", command=start_execution,
+                                  width=6)
     start_button.pack(side=tkinter.LEFT)
 
     time_step = tkinter.DoubleVar()
@@ -136,9 +141,11 @@ def main():
     scale = tkinter.Scale(frame, variable=time_speed, orient=tkinter.HORIZONTAL)
     scale.pack(side=tkinter.LEFT)
     
-    load_file_button = tkinter.Button(frame, text="Open file...", command=open_file_dialog)
+    load_file_button = tkinter.Button(frame, text="Open file...",
+                                      command=open_file_dialog)
     load_file_button.pack(side=tkinter.LEFT)
-    save_file_button = tkinter.Button(frame, text="Save to file...", command=save_file_dialog)
+    save_file_button = tkinter.Button(frame, text="Save to file...",
+                                      command=save_file_dialog)
     save_file_button.pack(side=tkinter.LEFT)
 
     displayed_time = tkinter.StringVar()
@@ -148,6 +155,7 @@ def main():
 
     root.mainloop()
     print('Modelling finished!')
+
 
 if __name__ == "__main__":
     main()
